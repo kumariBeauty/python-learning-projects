b4466a1d19f90b9d05f5fba90eb80e44db09cfb7
kumariBeauty <beautymgr5912@gmail.com>
2026-01-28 16:53:08 +0530

    input_output

---
+++ input_output.py
@@ -0,0 +1,5 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+height = input("Enter you height in feet:")
+
+print("Hello, "+name+"! You are "+age+" years old and "+height+" feet tall.")
